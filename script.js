082ee19e272cffcb08f67c9f11dc93f4f4647d4b
Saladin88 <145976634+Saladin88@users.noreply.github.com>
2023-10-30 23:08:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const button1 = document.getElementById('btn-1');
+const button2 = document.getElementById('btn-2');
+const button3 = document.getElementById('btn-3');
+const button4 = document.getElementById('btn-4');
+const answer = document.querySelector('p');
+const question = document.querySelector('h4');
+
+/////////////////////////////
+//creer un evement mouse 
+
+const mouse = document.querySelector('.mousemove');
+
+mouse.addEventListener('mouse')
